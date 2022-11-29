d00d255a0082fa891c5cf7af5a7d14e0742ea531
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-29 12:42:51 +0600

    Math.floor(Math.random() * tests.length)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const tests = require("./tests.json");
 
-app.get("/hello", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).json(tests);
+});
+
+app.get("/random", (req, res) => {
+  let index = Math.floor(Math.random() * tests.length);
+  let test = tests[index];
+  res.status(200).json(test);
 });
 
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
