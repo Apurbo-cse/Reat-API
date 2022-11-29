9c126daf0c05ad61137d5bbca2f946a889e872a0
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-29 14:32:48 +0600

    Update note.js

---
+++ src/models/note.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+  {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
