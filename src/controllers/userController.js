f5c234f2805b8f7adce8a9af21e00e931588ca75
Apurbo Kumar Anup <apurboka@gmail.com>
2022-11-29 14:52:31 +0600

    User Creation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,20 +2,27 @@ const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
 
 const signup = async (req, res) => {
-  // Existing user Check
-  // Hashed Password
-  // User Creation
   //Token Generate
 
   const { username, email, password } = req.body;
 
   try {
+    // Existing user Check
     const existingUser = await userModel.findOne({ email: email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    // Hashed Password
     const hasPassword = await bcrypt.hash(password, 10);
+
+    // User Creation
+    const result = await userModel.create({
+      email: email,
+      password: hasPassword,
+      username: username,
+    });
+    
   } catch (err) {}
 };
 
